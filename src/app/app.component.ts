7cf8437ac2118d1eca7613dcf307b307ae1ea7d3
andrantis <andresantista@gmail.com>
2019-06-17 21:11:37 -0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+// import { of } from 'rxjs/operators';
+
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
+  animations: [
+    //   trigger('EnterLeave', [
+    //     state('flyIn', style({ transform: 'translateX(0)' })),
+    //     transition(':enter', [
+    //       style({ transform: 'translateX(-100%)' }),
+    //       animate('0.5s 300ms ease-in')
+    //     ]),
+    //     transition(':leave', [
+    //       animate('0.3s ease-out', style({ transform: 'translateX(100%)' }))
+    //     ])
+    //   ])
+
+
+    // trigger('EnterLeave', [
+    //   state('flyIn', style({ transform: 'translateY(0)' })),
+    //   transition(':enter', [
+    //     style({ transform: 'translateY(-100%)' }),
+    //     animate('300ms ease-in')
+    //   ]),
+    //   transition(':leave', [
+    //     animate('0.3s ease-out', style({ transform: 'translateY(100%)' }))
+    //   ])
+    // ])
+
+
+
+    trigger('fadeInOut', [
+      state('void', style({
+        opacity: 0
+      })),
+      transition('void <=> *', animate(500)),
+    ])
+  ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular';
 
-  errors = [
-    {text: 'There was a problem loading account remarks', code: 1234},
-    {text: 'There was a problem loading account holds.', code: 12342}
-  ]
+  // errors: Observable<Array<any>>;
+  errors = [];
 
+  ngOnInit() {
+
+    const es = [
+      { text: 'There was a problem loading account remarks', code: 1234 },
+      { text: 'There was a problem loading account holds.', code: 5432 }
+    ];
+
+    setTimeout(() => {
+      this.errors = [...this.errors, es[0]];
+    }, 2000)
+
+
+    setTimeout(() => {
+      this.errors = [...this.errors, es[1]];
+    }, 5000)
+
+  }
 
   removeError(index: number) {
     this.errors = [...this.errors.slice(0, index), ...this.errors.slice(index + 1)];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MatToolbarModule, MatIconModule, MatButtonModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, MatToolbarModule, MatIconModule, MatButtonModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
